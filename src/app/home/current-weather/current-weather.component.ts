76c5839e8b0cda0b36ea2ac6cf9a6857f0af9822
jkaczmarek-p <j.kaczmarek-praktyki@info-baza.pl>
2023-11-30 15:01:53 +0100

    Ikony 30.11.2023

--- src/app/home/current-weather/current-weather.component.ts
+++ src/app/home/current-weather/current-weather.component.ts
@@ -9,7 +9,15 @@ export class CurrentWeatherComponent implements OnInit {
   @Input() temp: number | null = null;
   @Input() city: string | null = null;
 
+  weatherCondition: string = ''; // Deklaracja zmiennej przechowującej warunki pogodowe
+
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.weatherConditions(); // Wywołanie funkcji ustawiającej warunki pogodowe
+  }
+
+  weatherConditions() {
+    this.weatherCondition = 'sunny'; // Przypisanie wartości 'sunny' do zmiennej weatherCondition
+  }
 }
